17dd8f40f4fe45baf0f41b31f5adc20820c19c37
williamrm <william.pierre@net-c.com>
2021-02-09 15:28:49 +0100

    add docs folder

---
+++ docs/precache-manifest.66f88c551b0bc0da5368b6f3701b73f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "832725f893ba481605e6",
+    "url": "/vue-app-2/css/app.809e619f.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8538dad60b46360121f11ee04efb833f",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "6ef2e8b2b55f956b6a87",
+    "url": "/vue-app-2/js/about.970adf6f.js"
+  },
+  {
+    "revision": "832725f893ba481605e6",
+    "url": "/vue-app-2/js/app.518f092b.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
